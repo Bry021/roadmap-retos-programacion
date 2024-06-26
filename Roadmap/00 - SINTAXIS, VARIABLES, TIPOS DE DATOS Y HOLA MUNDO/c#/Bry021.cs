fffc9a80b52555d331a2b7f1aa57065c5cdc530d
Bryan Cuello <146749835+Bry021@users.noreply.github.com>
2024-06-26 03:55:28 -0400

    Bry021.cs

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/Bry021.cs
@@ -0,0 +1,34 @@
+using system;
+class Program
+{
+  
+  static void Main(string[] args)
+  {
+
+    // -Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+        // https://learn.microsoft.com/en-us/dotnet/csharp/
+
+    // -Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+        // las doble barra (//) que se utilizan para crear comentarios de una linea.
+        // la barra con asterisco (/* */) se utilizan para agregar comentarios de varias lineas.
+
+    // Crea una variable (y una constante si el lenguaje lo soporta).
+        int variable;
+        const int pi = 3.1416;
+
+    // Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos
+        int a = 52;
+        long b = 65;
+        float c = 32.5F;
+        double d = 1.2;
+        decimal e = 465.2;
+        string f = "good";
+        char g;    g = 'bad';
+        bool e = false;
+
+    // Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+        Console.Writeline("¡Hola, C#!");
+
+  }
+
+}
